82fc7d7566b3fa21777b1fc2dbf4132b3ee36fa5
Miguel Alvarez <malvarezl@royalresorts.com>
2022-11-16 15:11:11 -0500

    Se agrega unit test de filecurse repository

--- src/apps/mooc/backend/controllers/CoursePutController.ts
+++ src/apps/mooc/backend/controllers/CoursePutController.ts
@@ -1,11 +1,16 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import httpStatus from 'http-status';
-import { Controller } from './Controller';
+import {Controller} from './Controller';
+import {CourseCreator} from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
 
 export class CoursePutController implements Controller {
-  constructor() {}
+  constructor(private curseCreator: CourseCreator) {
+  }
 
   async run(req: Request, res: Response) {
+    const {id, name, duration} = req.body;
+
+    await this.curseCreator.run(id, name, duration);
     res.status(httpStatus.CREATED).send();
   }
 }
